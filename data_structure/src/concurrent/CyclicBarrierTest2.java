2ccd5991bd3738fe7b27924e1c9ba9c1c4023fe9
bxguo <951409002@qq.com>
2019-03-26 10:16:55 +0800

    test：CyclicBarrier线程回环栅栏测试 2

--- data_structure/src/concurrent/CyclicBarrierTest2.java
+++ data_structure/src/concurrent/CyclicBarrierTest2.java
@@ -1,7 +1,6 @@
 package concurrent;
 
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * @author: bxguo
@@ -11,16 +10,33 @@ public class CyclicBarrierTest2 {
     public static void main(String[] args) {
         //步数
         int count = 10;
-        final CyclicBarrier barrier = new CyclicBarrier(count + 1);
+        final CyclicBarrier barrier = new CyclicBarrier(count );
         int[] datas = new int[1024];
         //步大小
         int step = datas.length / count;
         for (int i = 0; i < count; i++) {
             int start = i * step;
-            int end = i == count - 1 ? datas.length - 1 : (i + 1) * step - 1;
+            int end = i == count - 1 ? datas.length - 1 : (i + 1) * step;
 
-
-            //ThreadPoolExecutor tp
+            ThreadPoolExecutor threadPool = new ThreadPoolExecutor(2, 4, 3,
+                    TimeUnit. SECONDS, new ArrayBlockingQueue<>(3),
+                    new ThreadPoolExecutor.DiscardOldestPolicy());
+            threadPool.execute(() -> {
+                System.out.println("分段排序中---");
+                try {
+                    Thread.sleep(1000);
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        try {
+            System.out.println("合并排序中---");
+            Thread.sleep(1000);
+            System.out.println("排序完成---");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
