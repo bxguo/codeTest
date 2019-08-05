5b9a2df59933ca99ad0521f16093ea84f4399dfa
bxguo <951409002@qq.com>
2019-08-05 12:35:50 +0800

    lintcode solution 12 字符串匹配(strstr只用声明不赋值)

--- data_structure/src/leetcode/Solution13.java
+++ data_structure/src/leetcode/Solution13.java
@@ -15,15 +15,11 @@ public class Solution13 {
         }
 
         for (int i = 0; i < source.length() - target.length() + 1; i++) {
-            int j = 0;
+            int j;
             for (j = 0; j < target.length(); j++) {
                 if (source.charAt(i + j) != target.charAt(j)) {
                     break;
                 }
-            }
-            // finished loop, target found
-            if (j == target.length()) {
-                return i;
             }
         }
         return -1;
