bf29f78a7c03f29564b83fc44d71d70a437bce3f
bxguo <951409002@qq.com>
2019-09-15 22:27:42 +0800

    6、没咋看，考研数学规律的
    Signed-off-by: bxguo <951409002@qq.com>

---
+++ data_structure/src/leetcode/Solution6.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+/**
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ *
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ * string convert(string s, int numRows);
+ * 示例 1:
+ *
+ * 输入: s = "LEETCODEISHIRING", numRows = 3
+ * 输出: "LCIRETOESIIGEDHN"
+ * 示例 2:
+ *
+ * 输入: s = "LEETCODEISHIRING", numRows = 4
+ * 输出: "LDREOEIIECIHNTSG"
+ * 解释:
+ *
+ * L     D     R
+ * E   O E   I I
+ * E C   I H   N
+ * T     S     G
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * Created by bxguo on 2019/9/15 22:17
+ */
+public class Solution6 {
+    public String convert(String s, int numRows) {
+        if(numRows == 1)
+            return s;
+
+        int len = Math.min(s.length(), numRows);
+        String []rows = new String[len];
+        for(int i = 0; i< len; i++) rows[i] = "";
+        int loc = 0;
+        boolean down = false;
+
+        for(int i=0;i<s.length();i++) {
+            rows[loc] += s.substring(i,i+1);
+            if(loc == 0 || loc == numRows - 1)
+                down = !down;
+            loc += down ? 1 : -1;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for(String row : rows) {
+            ans.append(row);
+        }
+        return ans.toString();
+    }
+}
