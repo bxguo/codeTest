95e6eb58506fd7c696fd617b516ee25ca455f184
bxguo <951409002@qq.com>
2019-09-28 12:27:17 +0800

    leecode 25 K 个一组翻转链表

---
+++ data_structure/src/leetcode/Solution25.java
@@ -0,0 +1,81 @@
+package leetcode;
+
+import leetcode.node.ListNode;
+
+/**
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * <p>
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ * 示例 :
+ * <p>
+ * 给定这个链表：1->2->3->4->5
+ * <p>
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ * <p>
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ * <p>
+ * 说明 :
+ * <p>
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * @author: bxguo
+ * @time: 2019/9/28 10:52
+ */
+public class Solution25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode t1 = new ListNode(1);
+        ListNode t2 = new ListNode(2);
+        ListNode t3 = new ListNode(3);
+        ListNode t4 = new ListNode(4);
+
+        t1.next = t2;
+        t1.next.next = t3;
+        t1.next.next.next = t4;
+
+        Solution25 solution25 = new Solution25();
+        solution25.reverseKGroup(t1, 2);
+
+    }
+}
---
+++ data_structure/src/testall/test1231.java
@@ -0,0 +1,19 @@
+package testall;
+
+/**
+ * @author: bxguo
+ * @time: 2019/9/27 16:52
+ */
+public class test1231 {
+    public static void main(String[] args) {
+        System.out.println((4 & 2) + ((4 ^ 2) >> 1));
+        System.out.println(((4 ^ 2) >> 1));
+        System.out.println(((4 + 2) >> 1));
+        System.out.println(((11 % 2)));
+        System.out.println(((11 & 1)));
+        System.out.println(((12 & 1)));
+        System.out.println(Math.log(10));
+        System.out.println(Math.pow(8.0, 3.0));
+    }
+
+}
