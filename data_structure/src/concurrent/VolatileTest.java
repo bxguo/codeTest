f559791cd86db9dbf24b41c3d9c28034fdfad14c
bxguo <951409002@qq.com>
2019-11-14 16:28:20 +0800

    volatile:修改可终止

--- data_structure/src/concurrent/VolatileTest.java
+++ data_structure/src/concurrent/VolatileTest.java
@@ -1,12 +1,14 @@
 package concurrent;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author: bxguo
  * @time: 2019/11/14 15:28
  */
 public class VolatileTest {
 
-    //加不加volatile两种不同的效果
+    //加不加 volatile  两种不同的效果
     private static volatile int value = 0;
     private static int MAX = 5;
 
@@ -26,9 +28,9 @@ public class VolatileTest {
             int temp = value;
             while (temp < MAX) {
                 try {
+                    Thread.sleep(1000);
                     System.out.println("value is change to :" + (++temp));
                     value = temp;
-                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
