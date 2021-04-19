2c1f4d2a8d8427cc442921039751e1cd6275151b
郭丙侠 <guobingxia@nn.com>
2021-04-19 10:39:01 +0800

    proc: 数组最大不连续递增子序列

---
+++ data_structure/src/leetcode/Solution221.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * Created by 郭炳侠 on 2021/4/17.
+ */
+public class Solution221 {
+
+    public static void main(String[] args) {
+        char[][] matrix = new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+        System.out.println(Solution221.maximalSquare(matrix));
+    }
+
+    public static int maximalSquare(char[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+            }
+        }
+        return 1;
+    }
+}
