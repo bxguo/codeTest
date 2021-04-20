1484aabda6fcee5f1170d264f6f38fb757e99d9b
郭丙侠 <guobingxia@nn.com>
2021-04-20 17:29:07 +0800

    test: 阻塞队列测试

---
+++ data_structure/src/testall/block/PCData.java
@@ -0,0 +1,21 @@
+package testall.block;
+
+/**
+ * Created by 郭炳侠 on 2021/4/20.
+ */
+public class PCData {
+    private final int intData;
+    public PCData(int d){
+        intData = d;
+    }
+    public PCData(String d){
+        intData = Integer.valueOf(d);
+    }
+    public int getData(){
+        return intData;
+    }
+    @Override
+    public String toString(){
+        return "data:"+intData;
+    }
+}
