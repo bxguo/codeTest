2a087b3537710468b35ca320477386ceaf156a6a
郭丙侠 <guobingxia@nn.com>
2021-04-22 14:06:34 +0800

    leetcode221: 最大正方形（动态规划）

--- data_structure/src/leetcode/Solution221.java
+++ data_structure/src/leetcode/Solution221.java
@@ -17,11 +17,24 @@ public class Solution221 {
     }
 
     public static int maximalSquare(char[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
             }
         }
-        return 1;
+        return maxSide * maxSide;
     }
 }
