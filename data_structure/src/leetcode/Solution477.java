d590faff0c4fd7a1a759d369f0f14a68061564e8
郭丙侠 <guobingxia@nn.com>
2021-06-28 17:42:01 +0800

    leetcode477: 最两个整数的 汉明距离 指的是这两个数字的二进制数对应位不同的数量。

---
+++ data_structure/src/leetcode/Solution477.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * 两个整数的 汉明距离 指的是这两个数字的二进制数对应位不同的数量。
+ * 计算一个数组中，任意两个数之间汉明距离的总和。
+ *
+ * Created by 郭炳侠 on 2021/5/28.
+ */
+public class Solution477 {
+    public int totalHammingDistance(int[] nums) {
+        return 0;
+    }
+}
